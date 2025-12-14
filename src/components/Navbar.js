1a04c169c9608a7b97af445fe9742794f03ffff0
Pooja <poojadnikam@coep.sveri.ac.in>
2025-12-14 07:41:11 +0530

    Configure GitHub Pages

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h2 className="logo">AgriMart</h2>
+
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/products">Products</Link>
+        <Link to="/cart">Cart</Link>
+        <Link to="/login">Login</Link>
+        
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Register.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Register.css";
+
+const Register = () => {
+  return (
+    <form className="Register-form">
+
+  
+    <div style={{ padding: "100px", backgroundColor:"rgba(137, 255, 34, 0.48)", borderRadius:"10px", maxWidth:"400px", margin:"auto", marginTop:"50px" }}>
+      <h1>Create Account</h1>
+      <input type="text" placeholder="Name" /><br /><br />
+      <input type="email" placeholder="Email" /><br /><br />
+      <input type="password" placeholder="Password" /><br /><br />
+      <button>Register</button>
+    </div>
+    </form>
+  );
+};
+
+export default Register;
